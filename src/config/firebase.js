26e024cdbc283022c372ffb584cd34d4186a616e
FarooqProProgrammer <farooqayub646@gmail.com>
2022-11-12 11:04:49 +0500

    2nd commit

---
+++ src/config/firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDEE54bpth99f4oE3ZH1pJZfb72WOo9oOY",
+
+  authDomain: "sylani-hackothon.firebaseapp.com",
+
+  projectId: "sylani-hackothon",
+
+  storageBucket: "sylani-hackothon.appspot.com",
+
+  messagingSenderId: "93908202893",
+
+  appId: "1:93908202893:web:6702dacdb786267ed9b9c4"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
